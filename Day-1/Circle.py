dcdd97e88cea413885cbbe93477f789e8e3fae29
alfisha-28 <ansarialfisha2006@gmail.com>
2025-05-29 12:45:50 +0530

    Basic input and variables

---
+++ Day-1/Circle.py
@@ -0,0 +1,14 @@
+#Area and circumference of Circle
+
+#Radius of circle
+radius = int(input("Enter radius of Circle : "))
+
+pi = 3.14 
+
+#Area of circle
+Area_of_circle = pi * radius * radius
+print(f"Area of circle with radius {radius} is {Area_of_circle}")
+
+#Circumference of circle
+Circumference_of_circle = 2 * pi * radius
+print(f"Circumference of circle with radius {radius} is {Circumference_of_circle}")
